1d5b43a839238f2f90d03ec6a1b7a8cf975d4261
Shubhansu kumar <shubhansu2021@gmail.com>
2022-11-09 23:40:49 +0530

    Integrated Modules
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-import encode
-import decode
+from tkinter import *
 
+import Gui.decrypterGUI as decrypterGUI
+import Gui.encrypterGUI as encrypterGUI
+
+master = Tk()
+master.geometry("600x600")
+master.title("Morse Code Translator")
+master.configure(background="orange")
+
+def trigger1(): 
+    encrypterGUI.openEncrypter(master)
+
+def trigger2():
+    decrypterGUI.openDecrypter(master)
+
+TitleLabel = Label(master, text="Morse Code Translator", font=("Arial", 20), bg="orange")
+TitleLabel.place(relx=0.28, rely=0.2)
+
+Encodebtn = Button(master, text="Encrypt", font=("Arial", 20), bg="white", command=trigger1)
+Encodebtn.place(relx=0.25, rely=0.4)
+
+Decodebtn = Button(master, text="Decrypt", font=("Arial", 20), bg="white", command=trigger2)
+Decodebtn.place(relx=0.60, rely=0.4)
+
+mainloop()
