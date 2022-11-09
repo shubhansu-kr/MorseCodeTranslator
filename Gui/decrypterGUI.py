d1324ef63f4093e2fc659d7024ac34be46218ccf
Shubhansu kumar <shubhansu2021@gmail.com>
2022-11-09 23:40:24 +0530

    Modularised GUI
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

---
+++ Gui/decrypterGUI.py
@@ -0,0 +1,47 @@
+from tkinter import *
+# master = Tk()
+
+# Import decode modules from decode.py
+import Algo.decode as decode 
+# Import files modules from files.py
+import File.files as files
+
+def openDecrypter(master):
+    decryptorWindow = Toplevel(master)
+    decryptorWindow.geometry("600x600")
+    decryptorWindow.title("Decrypter")
+    decryptorWindow.configure(background="orange")
+
+    TextEntryLabel = Label(decryptorWindow, text="Enter Text to Decrypt", font=("Arial", 13), bg="orange", fg="black")
+    TextEntryLabel.grid(row=0, column=1, pady = 8)
+
+    TextEntry = Entry(decryptorWindow, width=50, borderwidth=5)
+    TextEntry.grid(row=2, column=1, padx=120, pady=15)
+
+    # Event listener: Click Button trigger
+    def trigger(): 
+        s = TextEntry.get()
+        decodedString = decode.decodeMorse(s)
+
+        # Console log the string and write in the output file.
+        print('The String is :', decodedString)
+        files.writeOutput(decodedString)
+        
+        # Write the decrypted text in the text file. 
+        files.writeEnglish(files.readMorse)
+        
+        return decodedString
+
+    def showDecryptedText():
+        decryptedText = trigger()
+        DecryptedTextLabel = Label(decryptorWindow, text=decryptedText, font=("Arial", 25), bg="white", fg="black")
+        DecryptedTextLabel.place(height=174, relwidth=0.304, relx=0.348, rely=0.4)
+
+    DecryptButton = Button(decryptorWindow, text="Decrypt",command=showDecryptedText)
+    DecryptButton.place(relx=0.45, rely=0.15)
+
+    outputLabel = Label(decryptorWindow, text="Decrypted Text",font=("Arial", 20), bg="orange", fg="black")
+    outputLabel.place(height=174, relwidth=0.304, relx=0.348, rely=0.2)
+
+    OutputMessage = Message(decryptorWindow, text="", bg="white", fg="black")
+    OutputMessage.place(height=174, relwidth=0.6, relx=0.2, rely=0.4)
---
+++ Gui/encrypterGUI.py
@@ -0,0 +1,46 @@
+from tkinter import *
+# master = Tk()
+
+# Import encode module and files module
+import Algo.encode as encode 
+import File.files as files
+
+def openEncrypter(master):
+
+    encryptorWindow = Toplevel(master)
+    encryptorWindow.geometry("600x600")
+    encryptorWindow.title("Encrypter")
+    encryptorWindow.configure(background="orange")
+
+    TextEntryLabel = Label(encryptorWindow, text="Enter Text to Encrypt",font=("Arial", 13), bg="orange", fg="black")
+    TextEntryLabel.grid(row=0, column=1, pady = 8)
+
+    TextEntry = Entry(encryptorWindow, width=50, borderwidth=5)
+    TextEntry.grid(row=2, column=1, padx=120, pady=15)
+
+
+    def trigger(): 
+        s = TextEntry.get()
+        encodedString = encode.encodeMorse(s)
+
+        # Console log the string and write in the output file.
+        print('The Morse Code is :', encodedString)
+        files.writeOutput(encodedString)
+        
+        # Write the decrypted text in the text file. 
+        files.writeEnglish(files.readMorse)
+        
+        return encodedString
+
+    def showEncryptedText():
+        encryptedText = trigger()
+        EncryptedTextLabel = Label(encryptorWindow, text=encryptedText,font=("Arial", 25) , bg="white", fg="black")
+        EncryptedTextLabel.place(height=174, relwidth=0.304, relx=0.348, rely=0.4)
+
+    EncryptButton = Button(encryptorWindow, text="Encrypt",command=showEncryptedText)
+    EncryptButton.place(relx=0.45, rely=0.15)
+
+    outputLabel = Label(encryptorWindow, text="Encrypted Text",font=("Arial", 20), bg="orange", fg="black")
+    outputLabel.place(height=174, relwidth=0.304, relx=0.348, rely=0.2)
+    OutputMessage = Message(encryptorWindow, text="",bg="white", fg="black")
+    OutputMessage.place(height=174, relwidth=0.6, relx=0.2, rely=0.4)
